8e721cb7d5d22ba2c8c1dc7a84751e127e2111a5
my4776 <my4776@bs.amalnet.k12.il>
2025-02-01 13:29:18 +0200

    finis desin and finis create boons.

--- app/src/main/java/com/example/homework6/MainActivity.java
+++ app/src/main/java/com/example/homework6/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.homework6;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    ImageView iv;
+    Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.findViewById(R.id.btn);
+        iv.findViewById(R.id.iv);
+        rnd= new Random ();
+    }
+
+    public void Albert(View view) {
+
     }
 }
